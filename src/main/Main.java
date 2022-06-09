ac6df1d82919a5d9260f2d1133a3e8fe7e998b05
ederapenas <eder40k@live.com>
2022-06-09 07:52:55 -0300

    atualização do projeto, dessa vez finalmente funciona

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,50 +1,54 @@
 package main;
 
-import objetos.Hospital;
-import objetos.Medico;
-import objetos.Paciente;
-import java.util.LinkedList;
+import java.util.concurrent.Semaphore;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Scanner inputString = new Scanner(System.in);
-        LinkedList<Paciente> ordemPacientes = new LinkedList<Paciente>();
-        Medico medico = new Medico("Renato");
-        Hospital hospital = new Hospital(medico);
-        int op;
-        do{
-            System.out.println("Informe o nome do paciente: ");
-            String nome = inputString.nextLine();
-            System.out.println("Informe a doença do paciente: ");
-            String doenca = inputString.nextLine();
-            System.out.println("Informe o grau de risco (1 a 5)");
-            int grauDeRisco = input.nextInt();
-            while(grauDeRisco < 1 || grauDeRisco > 5){
-                System.out.println("Valor inválido, informe um grau de risco entre 1 e 5!");
-                grauDeRisco = input.nextInt();
-            }
-            Paciente paciente = new Paciente(nome, doenca, grauDeRisco);
-            hospital.addPacientes(paciente);
-            menu();
-            op = input.nextInt();
-            while(op < 0 || op > 1){
-                System.out.println("Valor inválido, digite 0 para não e 1 para sim.");
-                op = input.nextInt();
-            }
-        }while(op != 0);
+    static Semaphore semaforo = new Semaphore(4);
 
-        for(int i = 0; i < hospital.tamanhoFila(); i++){
-            hospital.run();
+    static class Paciente extends Thread{
+        String nome;
+        Paciente(String nome){
+            this.nome = nome;
+        }
+
+        public void run(){
+            try{
+                System.out.println("O numero de atendimentos disponiveis para " + nome + " é de: " + semaforo.availablePermits());
+                System.out.println(nome + " está esperando para ser atendido");
+                semaforo.acquire();
+                System.out.println(nome + " irá ser atendido!");
+
+                try{
+                    System.out.println(nome + " está sendo atendido nesse exato momento! Numero de atendimentos disponiveis: " + semaforo.availablePermits());
+                    Thread.sleep(3000);
+                }finally {
+                    System.out.println(nome + " foi atendido com sucesso!");
+                    semaforo.release();
+                    System.out.println("Numero de atendimentos disponiveis :" + semaforo.availablePermits());
+                }
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
         }
     }
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    public static void menu(){
-        System.out.println("Deseja criar mais um paciente?");
-        System.out.println("1 - Sim.");
-        System.out.println("0 - Não.");
+        System.out.println("Informe o numero de pacientes que você deseja ter: ");
+        int numeroPacientes = input.nextInt();
+        while(numeroPacientes <= 0){
+            System.out.println("Por favor, informe um numero maior que 0: ");
+            numeroPacientes = input.nextInt();
+        }
+
+        for(int i = 0; i < numeroPacientes; i++){
+            String nome = "Paciente " + i;
+            Paciente paciente = new Paciente(nome);
+            paciente.start();
+        }
     }
+        
 }
